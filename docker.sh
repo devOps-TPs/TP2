ffc2e48d3e5f78107250c7e626ade0ec0865e036
Grandkhan <kurt.cobain@tutanota.com>
2022-05-10 16:02:41 +0200

    dockersh update

--- docker.sh
+++ docker.sh
@@ -2,6 +2,7 @@ echo "Updating apt-get"
 sudo apt update -y
 
 echo "installing docker"
-sudo apt install -y docker.io
-sudo apt install -y docker-ce=18.03.0~ce-0~ubuntu
-docker info
+sudo apt install -y docker.io > /dev/null 2>&1
+sudo apt install -y docker-ce=18.03.0~ce-0~ubuntu > /dev/null 2>&1
+sudo apt install -y docker-compose > /dev/null 2>&1
+sudo docker info
